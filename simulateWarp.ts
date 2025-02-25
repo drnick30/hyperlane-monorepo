6e58988d175e8ddb8517df03e5d8135e8fa41e81
drnick30 <61126012+drnick30@users.noreply.github.com>
2025-02-25 01:50:48 +0100

    Update simulateWarp.ts

--- simulateWarp.ts
+++ simulateWarp.ts
@@ -1,6 +1,7 @@
 console.log("Simulating a warp route transaction...");
 
 // Example function
+
 function simulateTransaction(amount: number, sender: string, receiver: string) {
     console.log(`Sending ${amount} tokens from ${sender} to ${receiver}`);
 }
